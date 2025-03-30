cef962c4af7eec2f890d0394e2cd6683b90356a4
aparna-apz-07 <aparnamupparathil@gmail.com>
2025-03-30 16:19:21 +0530

    Update HotelDesc.js

--- Backend/Model/HotelDesc.js
+++ Backend/Model/HotelDesc.js
@@ -1,17 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const RoomSchema = new mongoose.Schema({
-//     HName: { type: String, required: true,unique: true},  // Hotel Name
-//     HAddress: { type: String, required: true },  // Hotel Address
-//     RPrice:{type: Number, required: true},  // Room Price
-//     RImage:{type : String, required: true},  // Room Image
-//     HDesc: { type: String, required: true}, // Hotel Description
-//     HotelEmail: { type: String, required: true } // link to hotel owner
-// })
-
-// const HotelDesc = mongoose.model('HotelDesc', RoomSchema);
-
-// module.exports = HotelDesc;
 
 const mongoose = require("mongoose");
 
@@ -21,7 +8,7 @@ const RoomSchema = new mongoose.Schema({
   RPrice: { type: Number, required: true },
   RImage: { type: String, required: true },
   HDesc: { type: String, required: true },
-  HotelEmail: { type: String, required: true }  // ✅ this must exist
+  HotelEmail: { type: String, required: true } 
 });
 
 module.exports = mongoose.model("HotelDesc", RoomSchema);
